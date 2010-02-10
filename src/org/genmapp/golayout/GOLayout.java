27c062ad2569430cd85d1aa9d529065c3f52ddef
Alexander Pico <apico@gladstone.ucsf.edu>
2010-02-10 01:56:11 +0000

    adds annotations via bridgerest (just adds regular go for proof of principle) uses tunables for input settings

--- src/org/genmapp/golayout/GOLayout.java
+++ src/org/genmapp/golayout/GOLayout.java
@@ -1,16 +1,29 @@
 package org.genmapp.golayout;
 
+import giny.model.Node;
+
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
+import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JSeparator;
+
+import org.bridgedb.BridgeDb;
+import org.bridgedb.DataSource;
+import org.bridgedb.IDMapper;
+import org.bridgedb.IDMapperException;
+import org.bridgedb.Xref;
+import org.bridgedb.bio.BioDataSource;
 
 import cytoscape.Cytoscape;
+import cytoscape.data.CyAttributes;
 import cytoscape.layout.AbstractLayout;
 import cytoscape.layout.CyLayoutAlgorithm;
 import cytoscape.layout.CyLayouts;
@@ -18,9 +31,11 @@ import cytoscape.layout.LayoutProperties;
 import cytoscape.layout.Tunable;
 import cytoscape.plugin.CytoscapePlugin;
 import cytoscape.view.CyNetworkView;
-import cytoscape.visual.VisualStyle;
 
 public class GOLayout extends CytoscapePlugin {
+
+	IDMapper _mapper = null;
+
 
 	/**
 	 * The constructor registers our layout algorithm. The CyLayouts mechanism
@@ -31,6 +46,92 @@ public class GOLayout extends CytoscapePlugin {
 		CyLayouts.addLayout(new GOLayoutAlgorithm(), "GO Layout");
 		CyLayouts.addLayout(new PartitionAlgorithm(), null);
 		CyLayouts.addLayout(new CellAlgorithm(), null);
+
+		JMenuItem item = new JMenuItem("Add GO-slim annotations");
+		JMenu layoutMenu = Cytoscape.getDesktop().getCyMenus().getMenuBar()
+				.getMenu("Layout");
+		item.addActionListener(new AddAnnotationCommandListener());
+
+		layoutMenu.add(item);
+	}
+
+	// Handles the top-level menu selection event from Cytoscape
+	public class AddAnnotationCommandListener implements ActionListener {
+
+		public AddAnnotationCommandListener() {
+
+		}
+
+		public void actionPerformed(ActionEvent evt_) {
+			setupBridgeDB(GOLayoutAlgorithm.annotationAtt,
+					GOLayoutAlgorithm.annotationCode,
+					GOLayoutAlgorithm.annotationSpecies);
+		}
+
+		public void setupBridgeDB(String annAtt, String annCode,
+				String annSpecies) {
+
+			DataSource annDs = DataSource.getByFullName(annCode);
+
+			try {
+				Class.forName("org.bridgedb.webservice.bridgerest.BridgeRest");
+			} catch (ClassNotFoundException e) {
+				System.out
+						.println("Can't register org.bridgedb.rdb.IDMapperRdb");
+				e.printStackTrace();
+			}
+
+			BioDataSource.init();
+			// now we connect to the driver and create a IDMapper instance.
+			// TODO: Update to use multiple species
+			try {
+				_mapper = BridgeDb
+						.connect("idmapper-bridgerest:http://webservice.bridgedb.org/"
+								+ annSpecies);
+			} catch (IDMapperException e) {
+				e.printStackTrace();
+			}  
+			System.out.println("ID Mapper = " + _mapper);
+			System.out.println("ID Mapper capabilites = "
+					+ _mapper.getCapabilities());
+
+			CyAttributes attrs = Cytoscape.getNodeAttributes();
+			Xref inXref = null;
+
+			Iterator<Node> it = Cytoscape.getCurrentNetwork().nodesIterator();
+			while (it.hasNext()) {
+				Node n = it.next();
+				String inputID = (String) Cytoscape.getNodeAttributes()
+						.getAttribute(n.getIdentifier(), annAtt);
+				if (null == inputID){
+					continue;
+				}
+
+				System.out.println("in: "+ inputID + " ds: "+ annDs + " full: "+annDs.getFullName());
+				inXref = new Xref(inputID, annDs);
+
+				Set<Xref> outRefs = null;
+				try {
+					outRefs = _mapper
+							.mapID(inXref, BioDataSource.GENE_ONTOLOGY);
+					// TODO: make 3 calls for each of the GO-Slims
+				} catch (IDMapperException e) {
+					System.out
+							.println("Got ID mapper exception trying to get mappings");
+				}
+				System.out.println("Got mappings: " + outRefs);
+				if (outRefs != null) {
+					List<String> attrList = new ArrayList<String>();
+					for (Xref outXref : outRefs) {
+						attrList.add(outXref.getId());
+					}
+					Cytoscape.getNodeAttributes().setListAttribute(
+							n.getIdentifier(), "annotation.FutureGOSlim",
+							attrList);
+				}
+
+			}
+		}
 	}
 
 	public static void createVisualStyle(CyNetworkView view) {
@@ -38,17 +139,33 @@ public class GOLayout extends CytoscapePlugin {
 
 	}
 
-	public class GOLayoutAlgorithm extends AbstractLayout {
+	public static class GOLayoutAlgorithm extends AbstractLayout {
 
 		protected static final String LAYOUT_NAME = "0-golayout";
 		LayoutProperties layoutProperties = null;
 		private static final String HELP = "GOLayout Help";
 
+		public static String annotationAtt = "entrezgene-id";
+		public static String annotationCode = null;
+		public static String annotationSpecies = null;
+		private List<String> codeValues = new ArrayList<String>();
+		private List<String> speciesValues = new ArrayList<String>();
+
+		
 		/**
 		 * Creates a new CellularLayoutAlgorithm object.
 		 */
 		public GOLayoutAlgorithm() {
 			super();
+			
+			
+
+			codeValues.add(BioDataSource.ENTREZ_GENE.getFullName());
+			codeValues.add(BioDataSource.ENSEMBL_HUMAN.getFullName());
+			speciesValues.add("Human");
+			speciesValues.add("Mouse");
+			speciesValues.add("Yeast");
+			
 			layoutProperties = new LayoutProperties(getName());
 			layoutProperties.add(new Tunable("global", "Global Settings",
 					Tunable.GROUP, new Integer(3)));
@@ -65,6 +182,20 @@ public class GOLayout extends CytoscapePlugin {
 					Tunable.NODEATTRIBUTE,
 					PartitionNetworkVisualStyleFactory.attributeName,
 					(Object) getInitialAttributeList(), (Object) null, 0));
+			layoutProperties.add(new Tunable("annotation",
+					"Annotation Settings", Tunable.GROUP, new Integer(3)));
+			layoutProperties.add(new Tunable("attributeAnnotation",
+					"The attribute to use for adding annotations",
+					Tunable.NODEATTRIBUTE, annotationAtt,
+					(Object) getInitialAttributeList(), (Object) null, 0));
+			layoutProperties.add(new Tunable("codeAnnotation",
+					"Type of identifier, e.g., Entrez Gene", Tunable.LIST,
+					new Integer(0), (Object) codeValues.toArray(),
+					null, 0));
+			layoutProperties.add(new Tunable("speciesAnnotation",
+					"Species", Tunable.LIST,
+					new Integer(0), (Object) speciesValues.toArray(),
+					null, 0));
 			layoutProperties.add(new Tunable("partition", "Partition Settings",
 					Tunable.GROUP, new Integer(2)));
 			layoutProperties.add(new Tunable("partitionMin",
@@ -92,7 +223,7 @@ public class GOLayout extends CytoscapePlugin {
 			 * our values based on what we read from the property file
 			 */
 			updateSettings(true);
-			
+
 			// Add help menu item
 			JMenuItem getHelp = new JMenuItem(HELP);
 			getHelp.setToolTipText("Open online help for GOLayout");
@@ -147,6 +278,24 @@ public class GOLayout extends CytoscapePlugin {
 				}
 			}
 
+			t = layoutProperties.get("attributeAnnotation");
+			if ((t != null) && (t.valueChanged() || force)){
+				String newValue = (String) t.getValue();
+				annotationAtt = newValue;
+			}
+			
+			t = layoutProperties.get("codeAnnotation");
+			if ((t != null) && (t.valueChanged() || force)){
+				String newValue = codeValues.get((Integer) t.getValue());
+				annotationCode = newValue;
+			}
+			
+			t = layoutProperties.get("speciesAnnotation");
+			if ((t != null) && (t.valueChanged() || force)){
+				String newValue = speciesValues.get((Integer) t.getValue());
+				annotationSpecies = newValue;
+			}
+			
 			t = layoutProperties.get("partitionMin");
 			if ((t != null) && (t.valueChanged() || force))
 				PartitionAlgorithm.NETWORK_LIMIT_MIN = ((Integer) t.getValue())
@@ -287,4 +436,3 @@ class GetHelpListener implements ActionListener {
 	}
 
 }
-
